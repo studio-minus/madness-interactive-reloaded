a4b4dab34fd69da276e3d2daf0d8167e3f4abcd2
zooi <32131519+mestiez@users.noreply.github.com>
2024-09-07 13:30:48 +0200

    Update DisclaimerRenderSystem.cs

--- src/MadnessInteractiveReloaded/DisclaimerRenderSystem.cs
+++ src/MadnessInteractiveReloaded/DisclaimerRenderSystem.cs
@@ -15,7 +15,7 @@ internal class DisclaimerRenderSystem : Walgelijk.System
         Draw.FontSize = 18;
         Draw.Colour = ((Color)Utilities.Lerp(Colors.Red, Colors.White, Easings.Expo.Out((Time.SecondsSinceLoadUnscaled * 0.1f) % 1))).WithAlpha(0.5f);
 
-        Draw.Text("BETA BUILD - YOU WILL ENCOUNTER BUGS AND UNFINISHED CONTENT\nPlease contribute to the repository :)", 
+        Draw.Text("BETA BUILD - YOU WILL ENCOUNTER BUGS, UNFINISHED CONTENT, LAG, AND CRASHING\nPlease contribute to the repository :)", 
             new Vector2(Window.Width * 0.5f, 10), Vector2.One, HorizontalTextAlign.Center, VerticalTextAlign.Top);
     }
 }
