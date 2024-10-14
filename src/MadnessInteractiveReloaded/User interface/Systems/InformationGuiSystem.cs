ad5536c98b53c25a49b0bb21f467016572e72292
zooi <32131519+mestiez@users.noreply.github.com>
2024-10-14 16:02:23 +0200

    make credits screen more complete, more easily edited

--- src/MadnessInteractiveReloaded/User interface/Systems/InformationGuiSystem.cs
+++ src/MadnessInteractiveReloaded/User interface/Systems/InformationGuiSystem.cs
@@ -1,5 +1,7 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using Walgelijk;
+using Walgelijk.AssetManager;
 using Walgelijk.Localisation;
 using Walgelijk.Onion;
 using Walgelijk.SimpleDrawing;
@@ -11,25 +13,8 @@ namespace MIR;
 /// </summary>
 public class InformationGuiSystem : Walgelijk.System
 {
-    private static readonly string infoText =
-$@"<color=#ff0000>Madness Interactive Reloaded v{GameVersion.Version} for {BuildInfo.Runtime}</color>
-<color=#ff0061>Studio Minus</color>
-
-Programming
-<color=#ffffff80>zooi</color>
-<color=#ffffff80>duston</color>
-<color=#ffffff80>Orsoniks</color>
-<color=#ffffff80>Orongto</color>
-
-Art and animations
-<color=#ffffff80>Dikkiedik</color>
-<color=#ffffff80>Koof</color>
-
-Original soundtrack
-<color=#ffffff80>Lothyde</color>
-<color=#ffffff80>Additional music by Cheshyre and mmodule.</color>
-
-<color=#ffffff80>Based on Madness Interactive by </color>Flecko (Max Abernethy)<color=#ffffff80> and Madness Combat by </color>Krinkels";
+    private static string startText = $"<color=#ff0000>Madness Interactive Reloaded v{GameVersion.Version} for {BuildInfo.Runtime}</color>\n<color=#ff0061>Studio Minus</color>";
+    private static string infoText => Assets.Load<string>("base:data/credits.txt");
 
     public override void Update()
     {
@@ -39,6 +24,10 @@ Original soundtrack
             Ui.Layout.FitWidth(true).Height(250).CenterHorizontal();
             Ui.Image(Textures.UserInterface.Logo.Value, ImageContainmentMode.Contain);
 
+            Ui.Layout.PreferredSize().FitWidth(true).CenterHorizontal();
+            Ui.Theme.FontSize(24).Once();
+            Ui.TextRect(startText, HorizontalTextAlign.Center, VerticalTextAlign.Top);   
+             
             Ui.Layout.PreferredSize().FitWidth(true).CenterHorizontal();
             Ui.Theme.FontSize(24).Once();
             Ui.TextRect(infoText, HorizontalTextAlign.Center, VerticalTextAlign.Top);
