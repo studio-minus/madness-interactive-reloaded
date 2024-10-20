a4e38113b36f62b57a42fa6e58ff775364440e5a
Koof <85030177+Koofa@users.noreply.github.com>
2024-10-20 23:58:23 +0200

    Added one handed gun melee animation for mags
    also added the ability to add custom one handed gun melee animations?

--- src/MadnessInteractiveReloaded/Character/CharacterStatsDeserialiser.cs
+++ src/MadnessInteractiveReloaded/Character/CharacterStatsDeserialiser.cs
@@ -42,6 +42,7 @@ public static class CharacterStatsDeserialiser
         deserialiser.RegisterStringArray("sword_seq", (s, v) => s.SwordSeq = [..v]);
         deserialiser.RegisterStringArray("two_handed_seq", (s, v) => s.TwoHandedSeq = [..v]);
         deserialiser.RegisterStringArray("two_handed_gun_seq", (s, v) => s.TwoHandedGunSeq = [..v]);
+        deserialiser.RegisterStringArray("one_handed_gun_seq", (s, v) => s.OneHandedGunSeq = [..v]);
         deserialiser.RegisterStringArray("blunt_seq", (s, v) => s.BluntSeq = [..v]);
         deserialiser.RegisterString("agility_skill_level", (s, v) => s.AgilitySkillLevel = Enum.Parse<AgilitySkillLevel>(v, true));
     }
