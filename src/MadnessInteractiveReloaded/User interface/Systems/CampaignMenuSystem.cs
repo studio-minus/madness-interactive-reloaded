2e5a35929920a1d49dd06477dbd696122300704f
zooi <32131519+mestiez@users.noreply.github.com>
2024-11-05 18:13:18 +0100

    localised campaign selection menu, improved appearance, fixed thumbnail scalign

--- src/MadnessInteractiveReloaded/User interface/Systems/CampaignMenuSystem.cs
+++ src/MadnessInteractiveReloaded/User interface/Systems/CampaignMenuSystem.cs
@@ -2,6 +2,7 @@
 using System.Numerics;
 using Walgelijk;
 using Walgelijk.AssetManager;
+using Walgelijk.Localisation;
 using Walgelijk.Onion;
 using Walgelijk.Onion.Controls;
 using Walgelijk.SimpleDrawing;
@@ -72,9 +73,25 @@ public class CampaignMenuSystem : Walgelijk.System
         MenuUiUtils.DrawBackground(Scene, 1);
         MenuUiUtils.DrawLogo(Scene, 1, 0);
 
+        // big boy in the center
+        {
+            var c = Window.Size * 0.5f;
+            Draw.ResetMaterial();
+            Draw.Colour = Colors.Red * 0.25f;
+            Draw.BlendMode = BlendMode.Addition;
+            Draw.TransformMatrix = Matrix3x2.CreateScale(-1, 1, c);
+            Draw.Material = Materials.BlurDraw;
+            Draw.Image(PlayerDrawTarget,
+                new Rect(0, 0, Window.Width, Window.Height * 1.5f), ImageContainmentMode.Contain);
+            Draw.ResetMaterial();
+            Draw.ResetTransformation();
+            Draw.BlendMode = BlendMode.AlphaBlend;
+            Draw.Colour = Colors.White;
+        }
+
         Ui.Theme.FontSize(35).Once();
         Ui.Layout.Width(400).Height(40).Move(13, 13).Move(0, 310 / Onion.GlobalScale);
-        Ui.TextRect("Campaigns", HorizontalTextAlign.Left, VerticalTextAlign.Top);
+        Ui.TextRect(Localisation.Get("main-menu-campaigns"), HorizontalTextAlign.Left, VerticalTextAlign.Top);
 
         Ui.Theme.Padding(10).ForegroundColor(Colors.Black.WithAlpha(0.5f)).OutlineWidth(1).Once();
         Ui.Layout.FitContainer().StickLeft().MaxWidth(400).Scale(0, -360 / Onion.GlobalScale).StickBottom().Scale(0, -50);
@@ -103,15 +120,18 @@ public class CampaignMenuSystem : Walgelijk.System
 
                     if (MadnessUtils.FindPlayer(Scene, out var player, out var character))
                     {
-                        bool needsLookUpdate = false;
+                        character.NeedsLookUpdate = true;
 
-                        if (c.Look == null && character.Look != UserData.Instances.PlayerLook) // requested look is none, but look isnt playerlook, so we reset
-                            needsLookUpdate = true;
+                        bool needsReset = false;
+
+                        if (c.Look == null && character.Look != UserData.Instances.PlayerLook)
+                            // requested look is none, but look isnt playerlook, so we reset
+                            needsReset = true;
 
                         if (c.Look != null && Registries.Looks.TryGet(c.Look, out var requested) && character.Look != requested)
-                            needsLookUpdate = true;
+                            needsReset = true;
 
-                        if (needsLookUpdate)
+                        if (needsReset)
                             character.Delete(Scene);
                     }
                 }
@@ -152,12 +172,12 @@ public class CampaignMenuSystem : Walgelijk.System
         Draw.Order = new RenderOrder(0, 1);
 
         PlayerDrawRect = MadnessUtils.FitImageRect(PlayerDrawTarget, campaignBoxRect.Translate(0, -campaignBoxRect.Height), ImageContainmentMode.Contain);
-        PlayerDrawRect = PlayerDrawRect.Translate(0, campaignBoxRect.MinY -  PlayerDrawRect.MaxY - pedestalHeight ); // stick to top of box
+        PlayerDrawRect = PlayerDrawRect.Translate(0, campaignBoxRect.MinY - PlayerDrawRect.MaxY - pedestalHeight); // stick to top of box
         Draw.Image(PlayerDrawTarget, PlayerDrawRect.Translate(0, pedestalHeight * 0.5f), ImageContainmentMode.Stretch);
 
         if (Input.IsButtonPressed(MouseButton.Left) && PlayerDrawRect.ContainsPoint(Input.WindowMousePosition))
             if (MadnessUtils.FindPlayer(Scene, out var player, out var character))
-                character.PlayAnimation(Registries.Animations.GetRandomValue());
+                character.PlayAnimation(Utilities.PickRandom(Animations.Dancing));
 
         Draw.Order = default;
         var pedestal = Assets.Load<Texture>("textures/pedestal.png").Value;
@@ -171,14 +191,16 @@ public class CampaignMenuSystem : Walgelijk.System
         Ui.Theme.ForegroundColor(Colors.Black.WithAlpha(0.5f)).OutlineWidth(1).Once();
         Ui.StartGroup();
         {
-            if (selectedCampaign != null && Registries.Campaigns.TryGet(selectedCampaign, out var sc) && sc != null)
+            if (selectedCampaign != null && Registries.Campaigns.TryGet(selectedCampaign, out var campaign) && campaign != null)
             {
                 if (CampaignProgress.TryGetCurrentStats(out var stats))
                 {
                     Ui.Layout.FitContainer(1, 0.7f).StickLeft().StickTop();
-                    Ui.Image(sc.Thumbnail.Value, ImageContainmentMode.Cover);
+                    Ui.Theme.BackgroundColor(Colors.Black).Once();
+                    Ui.Decorate(new BackgroundDecorator());
+                    Ui.Image(campaign.Thumbnail.Value, ImageContainmentMode.Contain);
 
-                    Ui.Layout.FitContainer(1, 0.3f).StickBottom().StickLeft();
+                    Ui.Layout.FitContainer(1, 0.3f).StickBottom().StickLeft().Scale(0, -40);
                     Ui.StartScrollView(false);
                     {
                         Ui.Layout.FitContainer(0.5f, null).Height(90).StickLeft().StickTop().VerticalLayout();
@@ -186,12 +208,12 @@ public class CampaignMenuSystem : Walgelijk.System
                         {
                             Ui.Layout.Height(40).FitWidth().StickLeft().StickTop();
                             Ui.Theme.FontSize(16).Once();
-                            int a = int.Min(stats.LevelIndex, sc.Levels.Length);
-                            Ui.TextRect($"<color=#ff0000>Completed levels</color>\n{a} / {sc.Levels.Length}", HorizontalTextAlign.Left, VerticalTextAlign.Middle);
+                            int a = int.Min(stats.LevelIndex, campaign.Levels.Length);
+                            Ui.TextRect($"<color=#ff0000>{Localisation.Get("cmpgn-menu-completed-levels")}</color>\n{a} / {campaign.Levels.Length}", HorizontalTextAlign.Left, VerticalTextAlign.Middle);
 
                             Ui.Layout.Height(40).FitWidth().StickLeft().StickTop();
                             Ui.Theme.FontSize(16).Once();
-                            Ui.TextRect($"<color=#ff0000>Time spent</color>\n{stats.TotalTimeSpent:hh\\:mm\\:ss}", HorizontalTextAlign.Left, VerticalTextAlign.Middle);
+                            Ui.TextRect($"<color=#ff0000>{Localisation.Get("cmpgn-menu-time-spent")}</color>\n{stats.TotalTimeSpent:hh\\:mm\\:ss}", HorizontalTextAlign.Left, VerticalTextAlign.Middle);
                         }
                         Ui.End();
 
@@ -200,34 +222,41 @@ public class CampaignMenuSystem : Walgelijk.System
                         {
                             Ui.Layout.Height(40).FitWidth().StickRight().StickTop();
                             Ui.Theme.FontSize(16).Once();
-                            Ui.TextRect($"<color=#ff0000>Kills</color>\n{stats.TotalKills}", HorizontalTextAlign.Right, VerticalTextAlign.Middle);
+                            //Ui.TextRect($"<color=#ff0000>Kills</color>\n{stats.TotalKills}",
+                            Ui.TextRect(string.Format(Localisation.Get("frmt-kills"), stats.TotalKills),
+                                HorizontalTextAlign.Right, VerticalTextAlign.Middle);
 
                             Ui.Layout.Height(40).FitWidth().StickRight().StickTop();
                             Ui.Theme.FontSize(16).Once();
-                            Ui.TextRect($"<color=#ff0000>Deaths</color>\n{stats.TotalDeaths}", HorizontalTextAlign.Right, VerticalTextAlign.Middle);
+                            //Ui.TextRect($"<color=#ff0000>Deaths</color>\n{stats.TotalDeaths}", 
+                            Ui.TextRect(string.Format(Localisation.Get("frmt-deaths"), stats.TotalDeaths),
+                                HorizontalTextAlign.Right, VerticalTextAlign.Middle);
                         }
                         Ui.End();
                     }
                     Ui.End();
 
                     Ui.Layout.Size(130, 40).StickLeft().StickBottom().Order(1);
-                    if (Ui.Button("Level select"))
+                    if (Ui.Button(Localisation.Get("cmpgn-menu-lvl-select")))
                     {
                         Game.Scene = LevelSelectionMenuScene.Load(Game);
                         MadnessUtils.Flash(Colors.Black, 0.2f);
                     }
 
+                    var btnKey = stats.LevelIndex == 0 ?
+                        "cmpgn-menu-play" :
+                        (stats.LevelIndex >= campaign.Levels.Length ? "cmpgn-menu-restart" : "cmpgn-menu-continue");
                     Ui.Layout.Size(130, 40).StickRight().StickBottom().Order(1);
-                    if (Ui.Button(stats.LevelIndex == 0 ? "Play" : "Continue"))
+                    if (Ui.Button(Localisation.Get(btnKey)))
                     {
                         // TODO ensure we cant press this button while loading a campaign
 
                         var i = stats.LevelIndex;
 
-                        if (i >= sc.Levels.Length) // we completed the campaign, restart
+                        if (i >= campaign.Levels.Length) // we completed the campaign, restart
                             i = 0;
 
-                        Game.Scene = LevelLoadingScene.Create(Game, Registries.Levels.Get(sc.Levels[i]).Level, SceneCacheSettings.NoCache);
+                        Game.Scene = LevelLoadingScene.Create(Game, Registries.Levels.Get(campaign.Levels[i]).Level, SceneCacheSettings.NoCache);
                         MadnessUtils.Flash(Colors.Black, 0.2f);
                     }
                 }
@@ -235,7 +264,8 @@ public class CampaignMenuSystem : Walgelijk.System
             else
             {
                 Ui.Layout.FitContainer().Center();
-                Ui.TextRect("Select a campaign on the left", HorizontalTextAlign.Center, VerticalTextAlign.Middle);
+                Ui.TextRect(Localisation.Get("cmpgn-menu-select-campaign"),
+                    HorizontalTextAlign.Center, VerticalTextAlign.Middle);
             }
         }
         Ui.End();
