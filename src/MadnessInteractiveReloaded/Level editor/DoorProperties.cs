studio-minus/madness-interactive-reloaded

commit 8929bc31b495ed78f6432b296860d78bd6bc45b3

implement #436 and subdivide door mesh

src/MadnessInteractiveReloaded/Level editor/DoorProperties.cs
@@ -1,4 +1,4 @@
-﻿using Newtonsoft.Json;
+using Newtonsoft.Json;
 using System;
 using System.Numerics;
 using Walgelijk;
@@ -9,7 +9,7 @@ namespace MIR.LevelEditor;
 /// <summary>
 /// Properties shared across the <see cref="DoorComponent"/> and the <see cref="LevelObjects.Door"/>
 /// </summary>
-public struct DoorProperties
+public record DoorProperties
 {
     /// <summary>
     /// The point to spawn the entities at
@@ -54,10 +54,25 @@ public struct DoorProperties
     public GlobalAssetId? Texture;
 
     /// <summary>
+    /// The sound to use when opening the door. Will fall back to default if null.
+    /// </summary>
+    public AssetRef<FixedAudioData>? OpenSound;
+
+    /// <summary>
+    /// The sound to use when closing the door. Will fall back to default if null.
+    /// </summary>
+    public AssetRef<FixedAudioData>? CloseSound;
+
+    /// <summary>
+    /// Duration of the door opening/closing animation in seconds
+    /// </summary>
+    public float AnimationDuration = 0.4f;
+
+    /// <summary>
     /// Returns <see cref="Texture"/> or <see cref="Textures.Door"/>
     /// </summary>
     [JsonIgnore]
-    public readonly Texture EffectiveTexture
+    public Texture EffectiveTexture
     {
         get
         {

src/MadnessInteractiveReloaded/Level editor/LevelObjects/Door.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Numerics;
 using Walgelijk;
 using Walgelijk.Onion;
@@ -22,11 +22,11 @@ public class Door : LevelObject, ITagged
     public DoorProperties Properties;
 
     private static readonly Vector2[] polygonVertexBuffer = new Vector2[4];
-    private Material previewMaterial;
+    private readonly Material previewMaterial;
 
     private TranslateDrag<Vector2> dragTopLeft, dragTopRight, dragBottomleft, dragBottomRight;
     [Flags]
-    private enum Corner : byte
+    private enum Corner
     {
         None = 0,
         TopRight = 1,
@@ -261,29 +261,82 @@ public class Door : LevelObject, ITagged
             Ui.Checkbox(ref Properties.IsPortal, "Portal door");
         }
 
-        Ui.Spacer(16);
-        Ui.Label("Texture");
-        Ui.Layout.FitWidth(false).Height(32);
-        MadnessUi.AssetPicker(Properties.Texture ?? default, id =>
         {
-            Editor.RegisterAction();
-
-            if (Textures.Door.Id == id)
-                Properties.Texture = null;
-            else
-                Properties.Texture = id;
-        }, static c => c.MimeType.Contains("image"));
-
-        if (Properties.Texture.HasValue)
-        {
+            Ui.Spacer(16);
+            Ui.Label("Texture");
             Ui.Layout.FitWidth(false).Height(32);
-            if (Ui.Button("Reset texture"))
+            MadnessUi.AssetPicker(Properties.Texture ?? default, id =>
             {
                 Editor.RegisterAction();
-                Properties.Texture = null;
+
+                if (Textures.Door.Id == id)
+                    Properties.Texture = null;
+                else
+                    Properties.Texture = id;
+            }, static c => c.MimeType.Contains("image"));
+
+            if (Properties.Texture.HasValue)
+            {
+                Ui.Layout.FitWidth(false).Height(32);
+                if (Ui.Button("Reset texture"))
+                {
+                    Editor.RegisterAction();
+                    Properties.Texture = null;
+                }
             }
         }
 
+        {
+            Ui.Spacer(16);
+            Ui.Label("Close sound");
+            Ui.Layout.FitWidth(false).Height(32);
+            MadnessUi.AssetPicker((Properties.CloseSound ?? default).Id, id =>
+            {
+                Editor.RegisterAction();
+                Properties.CloseSound = new(id);
+            }, static c => c.MimeType.Contains("audio"));
+
+            if (Properties.CloseSound.HasValue)
+            {
+                Ui.Layout.FitWidth(false).Height(32);
+                if (Ui.Button("Reset close sound"))
+                {
+                    Editor.RegisterAction();
+                    Properties.CloseSound = null;
+                }
+            }
+        }
+
+        {
+            Ui.Spacer(16);
+            Ui.Label("Open sound");
+            Ui.Layout.FitWidth(false).Height(32);
+            MadnessUi.AssetPicker((Properties.OpenSound ?? default).Id, id =>
+            {
+                Editor.RegisterAction();
+                Properties.OpenSound = new(id);
+            }, static c => c.MimeType.Contains("audio"));
+
+            if (Properties.OpenSound.HasValue)
+            {
+                Ui.Layout.FitWidth(false).Height(32);
+                if (Ui.Button("Reset open sound"))
+                {
+                    Editor.RegisterAction();
+                    Properties.OpenSound = null;
+                }
+            }
+        }
+
+        Ui.Spacer(16);
+        Ui.Label("Duration");
+        Ui.Layout.FitWidth(false).Height(32);
+        if (Ui.FloatInputBox(ref Properties.AnimationDuration, (0, 1)))
+        {
+            Editor.RegisterAction();
+        }
+
+        Ui.Spacer(16);
         Ui.Label("Animation");
         Ui.Layout.FitWidth(false).Height(32);
         if (Ui.EnumDropdown(ref Properties.Behaviour))
@@ -297,6 +350,7 @@ public class Door : LevelObject, ITagged
 
         if (Properties.IsPortal)
         {
+            Ui.Label("Portal destination");
             Properties.DestinationLevel ??= Utilities.PickRandom(Editor.LevelIds);
             int selectedIndex = Array.IndexOf(Editor.LevelIds, Properties.DestinationLevel);
 

src/MadnessInteractiveReloaded/Level/Components/DoorComponent.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Numerics;
 using Walgelijk;
 
@@ -6,13 +6,12 @@ namespace MIR;
 
 public class DoorComponent : Component, IDisposable
 {
-    public static readonly float AnimationTime = 0.4f; // if you want to change this you have to do that in door.frag as well
-
     // door.frag shader uniform names
     public const string TimeUniform = "time";
     public const string TimeSinceChangeUniform = "timeSinceChange";
     public const string IsOpenUniform = "isOpen";
     public const string DoorTypeUniform = "doorType";
+    public const string AnimationDurationUniform = "animationDuration";
 
     /// <summary>
     /// Is the door open? 
@@ -58,11 +57,16 @@ public class DoorComponent : Component, IDisposable
             return;
 
         IsOpen = true;
+        Material.SetUniform(AnimationDurationUniform, Properties.AnimationDuration);
         Material.SetUniform(IsOpenUniform, 1f);
         Material.SetUniform(TimeSinceChangeUniform, scene.Game.State.Time.SecondsSinceLoad);
         IsBusyWithAnimation = true;
-        MadnessUtils.DelayPausable(AnimationTime, () => IsBusyWithAnimation = false);
-        scene.Game.AudioRenderer.PlayOnce(Sounds.DoorOpen, 0.25f);
+        MadnessUtils.DelayPausable(Properties.AnimationDuration, () => IsBusyWithAnimation = false);
+
+        var snd = Sounds.DoorOpen;
+        if (Properties.OpenSound.TryGetValue(out var data))
+            snd = SoundCache.Instance.LoadSoundEffect(data);
+        scene.Game.AudioRenderer.PlayOnce(snd, 0.25f);
     }
 
     public void Close(Scene scene)
@@ -71,11 +75,16 @@ public class DoorComponent : Component, IDisposable
             return;
 
         IsOpen = false;
+        Material.SetUniform(AnimationDurationUniform, Properties.AnimationDuration);
         Material.SetUniform(IsOpenUniform, 0f);
         Material.SetUniform(TimeSinceChangeUniform, scene.Game.State.Time.SecondsSinceLoad);
         IsBusyWithAnimation = true;
-        MadnessUtils.DelayPausable(AnimationTime, () => IsBusyWithAnimation = false);
-        scene.Game.AudioRenderer.PlayOnce(Sounds.DoorClose, 0.25f);
+        MadnessUtils.DelayPausable(Properties.AnimationDuration, () => IsBusyWithAnimation = false);
+
+        var snd = Sounds.DoorClose;
+        if (Properties.CloseSound.TryGetValue(out var data))
+            snd = SoundCache.Instance.LoadSoundEffect(data);
+        scene.Game.AudioRenderer.PlayOnce(snd, 0.25f);
     }
 
     public void Dispose()

src/MadnessInteractiveReloaded/Prefabs/Prefabs.cs
@@ -1,4 +1,4 @@
-﻿using OpenTK.Graphics.OpenGL;
+using OpenTK.Graphics.OpenGL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -965,18 +965,41 @@ public static class Prefabs
     /// <exception cref="Exception"></exception>
     public static DoorComponent CreateDoor(Scene scene, in LevelEditor.DoorProperties properties)
     {
-        var vertices = new Vertex[4]
+        int horizontalDivisions = 5;
+        int numVerticesPerRow = horizontalDivisions + 1;
+        int vertexCount = numVerticesPerRow * 2;
+        var vertices = new Vertex[vertexCount];
+
+        for (int i = 0; i <= horizontalDivisions; i++)
         {
-            new Vertex(new Vector3(properties.BottomLeft, 0), new Vector2(0, 0), Colors.White),
-            new Vertex(new Vector3(properties.BottomRight, 0), new Vector2(1, 0), Colors.White),
-            new Vertex(new Vector3(properties.TopRight, 0), new Vector2(1, 1), Colors.White),
-            new Vertex(new Vector3(properties.TopLeft, 0), new Vector2(0, 1), Colors.White),
-        };
-        var indices = new uint[]
+            var t = (float)i / horizontalDivisions;
+            var bottom = Vector2.Lerp(properties.BottomLeft, properties.BottomRight, t);
+            var top = Vector2.Lerp(properties.TopLeft, properties.TopRight, t);
+
+            vertices[i * 2] = new Vertex(new(bottom, 0), new Vector2(t, 0), Colors.White);
+            vertices[i * 2 + 1] = new Vertex(new(top, 0), new Vector2(t, 1), Colors.White);
+        }
+
+        var indices = new uint[horizontalDivisions * 6];
+        int index = 0;
+
+        for (int i = 0; i < horizontalDivisions; i++)
         {
-            0u, 1u, 2u,
-            0u, 3u, 2u
-        };
+            var bottomLeft = (uint)(i * 2);
+            var bottomRight = (uint)(i * 2 + 2);
+            var topLeft = (uint)(i * 2 + 1);
+            var topRight = (uint)(i * 2 + 3);
+
+            indices[index++] = bottomLeft;
+            indices[index++] = bottomRight;
+            indices[index++] = topRight;
+
+            indices[index++] = bottomLeft;
+            indices[index++] = topRight;
+            indices[index++] = topLeft;
+        }
+
+
         var mesh = new VertexBuffer(vertices, indices);
         mesh.PrimitiveType = Primitive.Triangles;
 
@@ -984,7 +1007,7 @@ public static class Prefabs
         var entity = scene.CreateEntity();
         scene.AttachComponent(entity, new TransformComponent());
         var door = scene.AttachComponent(entity, new DoorComponent(mat, properties));
-        scene.AttachComponent(entity, new CustomShapeComponent(mesh, mat) //wordt automatisch gedisposed :)
+        scene.AttachComponent(entity, new CustomShapeComponent(mesh, mat) // wordt automatisch gedisposed :)
         {
             RenderOrder = RenderOrders.BackgroundBehind.WithOrder(1)
         });

src/MadnessInteractiveReloaded/Level/Systems/EnemySpawningSystem.cs
@@ -1,4 +1,4 @@
-﻿using MIR.LevelEditor.Objects;
+using MIR.LevelEditor.Objects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -97,7 +97,7 @@ public class EnemySpawningSystem : Walgelijk.System
         var wasPortal = door.Properties.IsPortal;
         door.Properties.IsPortal = false;
         door.Open(Scene);
-        yield return new GameSafeRoutineDelay(DoorComponent.AnimationTime * 1.5f);
+        yield return new GameSafeRoutineDelay(door.Properties.AnimationDuration * 1.5f);
 
         for (int i = 0; i < amount; i++)
         {
@@ -109,7 +109,7 @@ public class EnemySpawningSystem : Walgelijk.System
                 yield return new GameSafeRoutineDelay(Utilities.RandomFloat(0.1f, .4f));
         }
 
-        yield return new GameSafeRoutineDelay(DoorComponent.AnimationTime * 0.5f);
+        yield return new GameSafeRoutineDelay(door.Properties.AnimationDuration * 0.5f);
         door.Close(Scene);
         door.Properties.IsPortal = wasPortal;
     }

src/MadnessInteractiveReloaded/base/shaders/door.frag
@@ -1,5 +1,4 @@
 ﻿#version 330
-#define ANIMATION_DURATION 0.4
 
 in vec2 uv;
 in vec4 vertexColor;
@@ -12,6 +11,7 @@ uniform float isOpen;
 uniform sampler2D mainTex;
 uniform vec4 tint = vec4(1, 1, 1, 1);
 uniform float doorType = 0;
+uniform float animationDuration = 0.4;
 
 float easeInOutQuad(float x) {
     float z = -2.0 * x + 2.0;
@@ -38,7 +38,7 @@ float clampToZeroOrOne(float value) {
 
 void main()
 {
-    float progress = easeInOutQuad(clamp(time - timeSinceChange, 0.0, ANIMATION_DURATION) / ANIMATION_DURATION);
+    float progress = easeInOutQuad(clamp(time - timeSinceChange, 0.0, animationDuration) / animationDuration);
     progress = (isOpen > 0.5 ? progress : 1.0 - progress);
 	vec2 tUv = mix(verticalDoor(uv, progress), horizontalDoor(uv, progress), doorType);
     color = texture(mainTex, tUv) * tint * clampToZeroOrOne(tUv.x) * clampToZeroOrOne(tUv.y);

commit 4637df847642791b72e3d5e302097f862f39db28

fix bugs introduced by 8929bc31b495ed78f6432b296860d78bd6bc45b3 where undo would crash the game and increased door subdivision count

src/MadnessInteractiveReloaded/Level editor/DoorProperties.cs
@@ -56,12 +56,12 @@ public record DoorProperties
     /// <summary>
     /// The sound to use when opening the door. Will fall back to default if null.
     /// </summary>
-    public AssetRef<FixedAudioData>? OpenSound;
+    public GlobalAssetId? OpenSound;
 
     /// <summary>
     /// The sound to use when closing the door. Will fall back to default if null.
     /// </summary>
-    public AssetRef<FixedAudioData>? CloseSound;
+    public GlobalAssetId? CloseSound;
 
     /// <summary>
     /// Duration of the door opening/closing animation in seconds

src/MadnessInteractiveReloaded/Level editor/LevelObjects/Door.cs
@@ -290,10 +290,10 @@ public class Door : LevelObject, ITagged
             Ui.Spacer(16);
             Ui.Label("Close sound");
             Ui.Layout.FitWidth(false).Height(32);
-            MadnessUi.AssetPicker((Properties.CloseSound ?? default).Id, id =>
+            MadnessUi.AssetPicker(Properties.CloseSound ?? default, id =>
             {
                 Editor.RegisterAction();
-                Properties.CloseSound = new(id);
+                Properties.CloseSound = id;
             }, static c => c.MimeType.Contains("audio"));
 
             if (Properties.CloseSound.HasValue)
@@ -311,10 +311,10 @@ public class Door : LevelObject, ITagged
             Ui.Spacer(16);
             Ui.Label("Open sound");
             Ui.Layout.FitWidth(false).Height(32);
-            MadnessUi.AssetPicker((Properties.OpenSound ?? default).Id, id =>
+            MadnessUi.AssetPicker(Properties.OpenSound ?? default, id =>
             {
                 Editor.RegisterAction();
-                Properties.OpenSound = new(id);
+                Properties.OpenSound = id;
             }, static c => c.MimeType.Contains("audio"));
 
             if (Properties.OpenSound.HasValue)

src/MadnessInteractiveReloaded/Level editor/LevelObjects/RectangleObject.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Numerics;
 using Walgelijk;
 using Walgelijk.SimpleDrawing;
@@ -65,6 +65,8 @@ public abstract class RectangleObject : LevelObject, ITagged
         var mousePos = input.WorldMousePosition;
         if (input.IsKeyPressed(DragKey))
         {
+            Editor.RegisterAction();
+
             targetDragSide = Side.None;
 
             if (MathF.Abs(mousePos.Y - Rectangle.MaxY) <= scaledDragEdgeWidth)

src/MadnessInteractiveReloaded/Level/Components/DoorComponent.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Numerics;
 using Walgelijk;
+using Walgelijk.AssetManager;
 
 namespace MIR;
 
@@ -64,7 +65,7 @@ public class DoorComponent : Component, IDisposable
         MadnessUtils.DelayPausable(Properties.AnimationDuration, () => IsBusyWithAnimation = false);
 
         var snd = Sounds.DoorOpen;
-        if (Properties.OpenSound.TryGetValue(out var data))
+        if (Properties.OpenSound.HasValue && Assets.TryLoad<FixedAudioData>(Properties.OpenSound.Value, out var data))
             snd = SoundCache.Instance.LoadSoundEffect(data);
         scene.Game.AudioRenderer.PlayOnce(snd, 0.25f);
     }
@@ -82,7 +83,7 @@ public class DoorComponent : Component, IDisposable
         MadnessUtils.DelayPausable(Properties.AnimationDuration, () => IsBusyWithAnimation = false);
 
         var snd = Sounds.DoorClose;
-        if (Properties.CloseSound.TryGetValue(out var data))
+        if (Properties.CloseSound.HasValue && Assets.TryLoad<FixedAudioData>(Properties.CloseSound.Value, out var data))
             snd = SoundCache.Instance.LoadSoundEffect(data);
         scene.Game.AudioRenderer.PlayOnce(snd, 0.25f);
     }

src/MadnessInteractiveReloaded/Prefabs/Prefabs.cs
@@ -965,38 +965,38 @@ public static class Prefabs
     /// <exception cref="Exception"></exception>
     public static DoorComponent CreateDoor(Scene scene, in LevelEditor.DoorProperties properties)
     {
-        int horizontalDivisions = 5;
-        int numVerticesPerRow = horizontalDivisions + 1;
-        int vertexCount = numVerticesPerRow * 2;
+        int verticalDivisions = 64; // is this too much???
+        int numVerticesPerColumn = verticalDivisions + 1;
+        int vertexCount = numVerticesPerColumn * 2;
         var vertices = new Vertex[vertexCount];
 
-        for (int i = 0; i <= horizontalDivisions; i++)
+        for (int i = 0; i <= verticalDivisions; i++)
         {
-            var t = (float)i / horizontalDivisions;
-            var bottom = Vector2.Lerp(properties.BottomLeft, properties.BottomRight, t);
-            var top = Vector2.Lerp(properties.TopLeft, properties.TopRight, t);
+            var t = (float)i / verticalDivisions;
+            var left = Vector2.Lerp(properties.BottomLeft, properties.TopLeft, t);
+            var right = Vector2.Lerp(properties.BottomRight, properties.TopRight, t);
 
-            vertices[i * 2] = new Vertex(new(bottom, 0), new Vector2(t, 0), Colors.White);
-            vertices[i * 2 + 1] = new Vertex(new(top, 0), new Vector2(t, 1), Colors.White);
+            vertices[i * 2] = new Vertex(new(left, 0), new Vector2(0, t), Colors.White);
+            vertices[i * 2 + 1] = new Vertex(new(right, 0), new Vector2(1, t), Colors.White);
         }
 
-        var indices = new uint[horizontalDivisions * 6];
+        var indices = new uint[verticalDivisions * 6];
         int index = 0;
 
-        for (int i = 0; i < horizontalDivisions; i++)
+        for (int i = 0; i < verticalDivisions; i++)
         {
             var bottomLeft = (uint)(i * 2);
-            var bottomRight = (uint)(i * 2 + 2);
-            var topLeft = (uint)(i * 2 + 1);
+            var bottomRight = (uint)(i * 2 + 1);
+            var topLeft = (uint)(i * 2 + 2);
             var topRight = (uint)(i * 2 + 3);
 
             indices[index++] = bottomLeft;
-            indices[index++] = bottomRight;
+            indices[index++] = topLeft;
             indices[index++] = topRight;
 
             indices[index++] = bottomLeft;
             indices[index++] = topRight;
-            indices[index++] = topLeft;
+            indices[index++] = bottomRight;
         }
 
 