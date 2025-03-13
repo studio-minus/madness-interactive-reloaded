b9e8b42e45f9a534bea543f57492143315ba1382
zooi <32131519+mestiez@users.noreply.github.com>
2025-03-13 14:48:56 +0100

    death animation gets slower the bigger the character is, and vice versa

--- src/MadnessInteractiveReloaded/Character/CharacterUtilities.cs
+++ src/MadnessInteractiveReloaded/Character/CharacterUtilities.cs
@@ -245,12 +245,14 @@ public static class CharacterUtilities
                 return;
             }
 
+            float speed = 1f / character.Positioning.Scale;
+            float duration = anim.TotalDuration * speed;
             character.StopAllAnimations();
-            var a = character.PlayAnimation(anim, 1);
+            var a = character.PlayAnimation(anim, speed);
 
             // during the animation, check if the body is above a flat ground. if it isnt, turn into a ragdoll MID ANIMATION 
             // TODO this should be in a system, not in a routine
-            MadnessUtils.RoutineForSecondsPausable(anim.TotalDuration, dt =>
+            MadnessUtils.RoutineForSecondsPausable(duration, dt =>
             {
                 var isFlatUnderMe = Level.CurrentLevel?.IsFlatAt(character.Positioning.Body.ComputedVisualCenter.X) ?? true;
                 if (!isFlatUnderMe && !character.AnimationConstrainsAny(AnimationConstraint.PreventRagdoll))
@@ -259,7 +261,7 @@ public static class CharacterUtilities
             });
             // if the animation is over stamp it onto the background immediately
             //a.OnEnd += () => // OnEnd is invoked at an unfortunate time, where the rendertasks are off by a few frames??
-            MadnessUtils.DelayPausable(anim.TotalDuration, () =>
+            MadnessUtils.DelayPausable(duration, () =>
             {
                 if (!character.HasBeenRagdolled && scene.HasEntity(entity))
                 {
